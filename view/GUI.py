4885fad5bf76ef11e716004d3de11de5bc62b313
AmandaIsrael <amanda_israel@hotmail.com>
2021-07-26 20:40:40 -0300

    get Entry
    Co-authored-by: Leonardo Vanzin <EnergyFall266@users.noreply.github.com>

--- view/GUI.py
+++ view/GUI.py
@@ -44,6 +44,9 @@ class CanvasMenu(Frame):
     # SideBar para definição do mundo e do objeto
     def initSideBar(self):
 
+       # def newWorld():
+            
+
         sideBar = LabelFrame(self.master, relief=FLAT)
 
         # Criação do canvas com a barra de rolamento
@@ -67,6 +70,9 @@ class CanvasMenu(Frame):
         #  Adição dos widgets no frame 
         labelWorldLimit = Label(scrollableFrame, text="Limite do mundo:", justify=LEFT, anchor="w", font=('Helvetica', 10, 'bold'), bg='#E0E0E0')
         labelWorldLimit.grid(row=0, column=0, padx=15, pady=10, columnspan=3, sticky=W)
+
+        labelWarning = Label(scrollableFrame, text="(Limite máximo: 980x640)", justify=LEFT, anchor="w", font=('Helvetica', 10), bg='#E0E0E0')
+        labelWarning.grid(row=0, column=2, padx=15, pady=10, columnspan=4, sticky=W)
 
         labelWorldLimitX1 = Label(scrollableFrame, text="X1", font=('Helvetica', 9), bg='#E0E0E0')
         labelWorldLimitX1.grid(row=1, column=0, padx=15, pady=10)
@@ -155,86 +161,88 @@ class CanvasMenu(Frame):
         coorFocalPointZ = Entry(scrollableFrame, width=8)
         coorFocalPointZ.grid(row=10, column=1, padx=15, pady=10)
 
-        labelObjectCenter = Label(scrollableFrame, text="Centro do objeto:", justify=LEFT, anchor="w", font=('Helvetica', 10, 'bold'), bg='#E0E0E0')
-        labelObjectCenter.grid(row=7, column=2, padx=30, pady=10, columnspan=3, sticky=W)
-
-        labelObjectCenterX = Label(scrollableFrame, text="X", font=('Helvetica', 9), bg='#E0E0E0')
-        labelObjectCenterX.grid(row=8, column=2, padx=15, pady=10)
-
-        coorObjectCenterX = Entry(scrollableFrame, width=8)
-        coorObjectCenterX.grid(row=8, column=3, padx=15, pady=10)
-
-        labelObjectCenterY = Label(scrollableFrame, text="Y", font=('Helvetica', 9), bg='#E0E0E0')
-        labelObjectCenterY.grid(row=9, column=2, padx=15, pady=10)
-
-        coorObjectCenterY = Entry(scrollableFrame, width=8)
-        coorObjectCenterY.grid(row=9, column=3, padx=15, pady=10)
-
-        labelObjectCenterZ = Label(scrollableFrame, text="Z", font=('Helvetica', 9), bg='#E0E0E0')
-        labelObjectCenterZ.grid(row=10, column=2, padx=15, pady=10)
-
-        coorObjectCenterZ = Entry(scrollableFrame, width=8)
-        coorObjectCenterZ.grid(row=10, column=3, padx=15, pady=10)
-
         labelDistance = Label(scrollableFrame, text="Distância:", justify=LEFT, anchor="w", font=('Helvetica', 10, 'bold'), bg='#E0E0E0')
-        labelDistance.grid(row=11, column=0, padx=15, pady=8, columnspan=3, sticky=W)
+        labelDistance.grid(row=7, column=2, padx=15, pady=8, columnspan=3, sticky=W)
 
-        labelProjectionPlane = Label(scrollableFrame, text="Plano de projeção", justify=LEFT, anchor="w", font=('Helvetica', 9), bg='#E0E0E0')
-        labelProjectionPlane.grid(row=12, column=0, padx=15, pady=8, columnspan=3, sticky=W)
+        labelProjectionPlane = Label(scrollableFrame, text="Plano de\nprojeção", justify=LEFT, anchor="w", font=('Helvetica', 9), bg='#E0E0E0')
+        labelProjectionPlane.grid(row=8, column=2, padx=15, pady=8, columnspan=3, sticky=W)
 
         coorProjectionPlane = Entry(scrollableFrame, width=8)
-        coorProjectionPlane.grid(row=12, column=2, padx=15, pady=8)
+        coorProjectionPlane.grid(row=8, column=3, padx=15, pady=8)
 
-        labelNearPlane = Label(scrollableFrame, text="Plano near", justify=LEFT, anchor="w", font=('Helvetica', 9), bg='#E0E0E0')
-        labelNearPlane.grid(row=13, column=0, padx=15, pady=8, columnspan=3, sticky=W)
+        labelNearPlane = Label(scrollableFrame, text="Plano near", justify=CENTER, anchor="w", font=('Helvetica', 9), bg='#E0E0E0')
+        labelNearPlane.grid(row=9, column=2, padx=15, pady=8, columnspan=3, sticky=W)
 
         coorNearPlane = Entry(scrollableFrame, width=8)
-        coorNearPlane.grid(row=13, column=2, padx=15, pady=8)
+        coorNearPlane.grid(row=9, column=3, padx=15, pady=8)
 
         labelFarPlane = Label(scrollableFrame, text="Plano far", justify=LEFT, anchor="w", font=('Helvetica', 9), bg='#E0E0E0')
-        labelFarPlane.grid(row=14, column=0, padx=15, pady=8, columnspan=3, sticky=W)
+        labelFarPlane.grid(row=10, column=2, padx=15, pady=8, columnspan=3, sticky=W)
 
         coorFarPlane = Entry(scrollableFrame, width=8)
-        coorFarPlane.grid(row=14, column=2, padx=15, pady=8)  
+        coorFarPlane.grid(row=10, column=3, padx=15, pady=8) 
+
+        novoMundo = Button(scrollableFrame, text="Novo mundo", font=('Helvetica', 10), bg='#edb1ba', width=15)#, command = newWorld)
+        novoMundo.grid(row=11, column=1, columnspan=3, pady= 30)
+
+        labelObjectCenter = Label(scrollableFrame, text="Centro do objeto:", justify=LEFT, anchor="w", font=('Helvetica', 10, 'bold'), bg='#E0E0E0')
+        labelObjectCenter.grid(row=12, column=3, pady=10, padx= 15, columnspan=3, sticky=W)
+
+        labelObjectCenterX = Label(scrollableFrame, text="X", font=('Helvetica', 9), bg='#E0E0E0')
+        labelObjectCenterX.grid(row=13, column=3, pady=10)
+
+        coorObjectCenterX = Entry(scrollableFrame, width=8)
+        coorObjectCenterX.grid(row=13, column=4, padx=5, pady=10)
+
+        labelObjectCenterY = Label(scrollableFrame, text="Y", font=('Helvetica', 9), bg='#E0E0E0')
+        labelObjectCenterY.grid(row=14, column=3, pady=10)
+
+        coorObjectCenterY = Entry(scrollableFrame, width=8)
+        coorObjectCenterY.grid(row=14, column=4, padx=5, pady=10)
+
+        labelObjectCenterZ = Label(scrollableFrame, text="Z", font=('Helvetica', 9), bg='#E0E0E0')
+        labelObjectCenterZ.grid(row=15, column=3, pady=10)
+
+        coorObjectCenterZ = Entry(scrollableFrame, width=8)
+        coorObjectCenterZ.grid(row=15, column=4, padx=5, pady=10) 
 
         labelObject = Label(scrollableFrame, text="Objeto:", justify=LEFT, anchor="w", font=('Helvetica', 10, 'bold'), bg='#E0E0E0')
-        labelObject.grid(row=16, column=0, padx=15, pady=8, columnspan=3, sticky=W)
+        labelObject.grid(row=12, column=0, padx=15, pady=8, columnspan=3, sticky=W)
 
         labelBaseRadius = Label(scrollableFrame, text="Raio da base", justify=LEFT, anchor="w", font=('Helvetica', 9), bg='#E0E0E0')
-        labelBaseRadius.grid(row=17, column=0, padx=15, pady=8, columnspan=3, sticky=W)
+        labelBaseRadius.grid(row=13, column=0, padx=15, pady=8, columnspan=3, sticky=W)
 
         coorBaseRadius = Entry(scrollableFrame, width= 8)
-        coorBaseRadius.grid(row=17, column=2, padx=15, pady=8)
+        coorBaseRadius.grid(row=13, column=2, padx=15, pady=8)
 
         labelTopRadius = Label(scrollableFrame, text="Raio do topo", justify=LEFT, anchor="w", font=('Helvetica', 9), bg='#E0E0E0')
-        labelTopRadius.grid(row=18, column=0, padx=15, pady=8, columnspan=3, sticky=W)
+        labelTopRadius.grid(row=14, column=0, padx=15, pady=8, columnspan=3, sticky=W)
 
         coorTopRadius = Entry(scrollableFrame, width= 8)
-        coorTopRadius.grid(row=18, column=2, padx=15, pady=8)
+        coorTopRadius.grid(row=14, column=2, padx=15, pady=8)
 
         labelNumSides = Label(scrollableFrame, text="Número de lados", justify=LEFT, anchor="w", font=('Helvetica', 9), bg='#E0E0E0')
-        labelNumSides.grid(row=19, column=0, padx=15, pady=8, columnspan=3, sticky=W)
+        labelNumSides.grid(row=15, column=0, padx=15, pady=8, columnspan=3, sticky=W)
 
         coorNumLados = Entry(scrollableFrame, width= 8)
-        coorNumLados.grid(row=19, column=2, padx=15, pady=8)
+        coorNumLados.grid(row=15, column=2, padx=15, pady=8)
 
         labelObjHeight = Label(scrollableFrame, text="Altura do objeto", justify=LEFT, anchor="w", font=('Helvetica', 9), bg='#E0E0E0')
-        labelObjHeight.grid(row=20, column=0, padx=15, pady=8, columnspan=3, sticky=W)
+        labelObjHeight.grid(row=16, column=0, padx=15, pady=8, columnspan=3, sticky=W)
 
         coorObjHeight = Entry(scrollableFrame, width= 8)
-        coorObjHeight.grid(row=20, column=2, padx=15, pady=8)
+        coorObjHeight.grid(row=16, column=2, padx=15, pady=8)
 
         novoObjeto = Button(scrollableFrame, text="Novo objeto", font=('Helvetica', 10), bg='#edb1ba', width=15, command = newObject)
-        novoObjeto.grid(row=21, column=0, columnspan=3, pady=50)
+        novoObjeto.grid(row=17, column=0, columnspan=3, pady=50)
 
         limparCena = Button(scrollableFrame, text="Limpar cena", font=('Helvetica', 10), bg='#edb1ba', width=15, command = newObject)
-        limparCena.grid(row=21, column=3, columnspan=3, pady=50)
+        limparCena.grid(row=17, column=3, columnspan=3, pady=50)
 
         sideBar.pack(side=RIGHT, fill=Y)
         canvas.pack(side=LEFT, fill=BOTH, expand=True)
         scrollBar.pack(side=RIGHT, fill=Y)
 
-    
     def initScreen(self):
             screen = Frame(self.master, highlightbackground='gray', highlightthickness=1)
             screen.rowconfigure(0, weight = 1)
@@ -243,6 +251,7 @@ class CanvasMenu(Frame):
             canvas = Canvas(screen)
 
             screen.place(x=10, y= 70, width=980, height=640)
+
             canvas.grid(sticky="nsew")
 
             canvas.bind("<Button-1>", locate_xy)
