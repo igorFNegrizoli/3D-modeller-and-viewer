742852080b3fe70add78e893775ba67fabb647d7
IgorFNegrizoli <igor.negrizoli@gmail.com>
2021-07-29 18:22:40 -0300

    Translate and escalate functions working, rotations are not done

---
+++ model/transformations.py
@@ -0,0 +1,37 @@
+import openmesh as om
+from math import sin, cos
+
+def translate(mesh, dx=0, dy=0, dz=0):
+	for vh in mesh.vertices():
+		mesh.point(vh)[0] += dx
+		mesh.point(vh)[1] += dy
+		mesh.point(vh)[2] += dz
+	return mesh
+
+def escalate(mesh, Sx=1, Sy=1, Sz=1):
+	for vh in mesh.vertices():
+		mesh.point(vh)[0] *= Sx
+		mesh.point(vh)[1] *= Sy
+		mesh.point(vh)[2] *= Sz
+	return mesh
+
+def rotX(mesh, angle):
+	for vh in mesh.vertices():
+		coord = mesh.point(vh)
+		mesh.point(vh)[1] = coord[1]*cos(angle) - coord[2]*sin(angle)
+		mesh.point(vh)[2] = coord[1]*sin(angle) + coord[2]*cos(angle)
+	return mesh
+
+def rotY(mesh, angle):
+	for vh in mesh.vertices():
+		coord = mesh.point(vh)
+		mesh.point(vh)[0] = coord[0]*cos(angle) + coord[2]*sin(angle)
+		mesh.point(vh)[2] = coord[2]*cos(angle) - coord[0]*sin(angle)
+	return mesh	
+
+def rotZ(mesh, angle):
+	for vh in mesh.vertices():
+		coord = mesh.point(vh)
+		mesh.point(vh)[0] = coord[0]*cos(angle) - coord[1]*sin(angle)
+		mesh.point(vh)[1] = coord[0]*sin(angle) + coord[1]*cos(angle)
+	return mesh
